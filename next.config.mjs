63af5fb5e6df9ece6a90aa0610609f1a15426133
tnetennba999 <sallycheung512@gmail.com>
2024-09-27 19:13:33 -0400

    GitHub page deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    basePath: '/lil-connections', // Change this to '/app-2' for the second app
+  assetPrefix: '/lil-connections/', // Same change for the second app
+  trailingSlash: true, // This is necessary for static export
+};
 
 export default nextConfig;
