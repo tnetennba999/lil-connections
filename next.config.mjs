a2abd9f53410a4c5b95e23084fc7b555cfbd5508
tnetennba999 <sallycheung512@gmail.com>
2024-09-27 19:22:45 -0400

    deleted export because we cant use it anymore

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
     basePath: '/lil-connections', // Change this to '/app-2' for the second app
   assetPrefix: '/lil-connections/', // Same change for the second app
   trailingSlash: true, // This is necessary for static export
+  output: 'export'
 };
 
 export default nextConfig;
